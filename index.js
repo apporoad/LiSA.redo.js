de8b126c30b269904e5ed1e3a98595f8b2478778
apporoad <apporoadina@163.com>
2019-12-09 20:44:09 +0800

    part

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+var Redo=function(path){
+    var _this = this
+    _this.submit= _this.commit= _this.put = _this.push= _this.one = ()=>{
+
+    }
+    _this.method = _this.how2do = _this.how = _this.two= _this.howtodo=()=>{
+
+    }
+    _this.do=_this.run=_this.go=()=>{
+
+    }
+}
+
+
+
+module.exports = Redo
