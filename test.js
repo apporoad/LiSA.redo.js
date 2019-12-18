6d01fc59274b780b83b2672676bead249837a344
apporoad <apporoadina@163.com>
2019-12-18 13:28:48 +0800

    some

--- test.js
+++ test.js
@@ -2,7 +2,11 @@ var test=require('./index')(__dirname+'/test.redo').order()
 
 test.handler((params,cb)=>{
     console.log(params)
-    cb("hello")
+    //cb("hello")
+    // return "hello"
+    return new Promise((r,j)=>{
+        r('hello')
+    })
 },(result,redo)=>{
     console.log('here is callback ' , result)
     console.log('redo :', redo)
